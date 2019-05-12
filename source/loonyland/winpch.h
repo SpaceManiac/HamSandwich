60d303f6825b3b646332f3076d7f20b9bcb91ec7
Tad Hardesty <tad@platymuus.com>
2019-05-12 02:48:27 -0700

    Fix Loonyland fonts not loading due to packing

--- source/loonyland/winpch.h
+++ source/loonyland/winpch.h
@@ -4,9 +4,6 @@
 // this header is just meant to be precompiled for compilation speed reasons.
 // mainly for the purposes of making it fast when windows.h is involved.
 #define WIN32_LEAN_AND_MEAN
-#pragma pack(1)
-#pragma warning( disable : 4800 4103 )
-
 #include <windows.h>
 #include "jamultypes.h"
 #include <string.h>
