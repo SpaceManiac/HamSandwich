6b9564a03146c3ae4e5c604bfd2b5233bb230000
Tad Hardesty <tad@platymuus.com>
2019-05-30 01:17:35 -0700

    Remove dead code in main

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-//#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_ttf.h>
 #include <string>
-#include <unistd.h>  // for chdir
 #include "globals.h"
 #include "editor.h"
 #include "resources.rc.h"
@@ -20,10 +19,6 @@ int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
     IMG_Init(IMG_INIT_PNG);
     if (TTF_Init()) printf("TTF_Init: %s\n", TTF_GetError());
-
-    /*string exe = argv[0];
-    string dir = exe.substr(0, exe.find_last_of("\\/"));
-    chdir(dir.c_str());*/
 
     gFont = TTF_OpenFontRW(EmbeddedRW(vera), true, 14);
     if (!gFont) printf("vera.ttf: %s\n", TTF_GetError());
