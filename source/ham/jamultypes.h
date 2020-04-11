f5142eed2dad448b5a1b2c286622e43c630e3291
Tad Hardesty <tad@platymuus.com>
2020-04-10 18:30:57 -0700

    Use stdint types to get correct byte/word/dword sizes

--- source/ham/jamultypes.h
+++ source/ham/jamultypes.h
@@ -3,11 +3,13 @@
 #ifndef JAMTYPES_H
 #define JAMTYPES_H
 
+#include <stdint.h>
+
 // Global typedefs and basic compiler compatibility stuff
 
-typedef unsigned char	byte;
-typedef unsigned short	word;
-typedef unsigned long	dword;
+typedef uint8_t  byte;
+typedef uint16_t word;
+typedef uint32_t dword;
 
 #ifndef __GNUC__
 #define strcasecmp _stricmp
