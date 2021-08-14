41258ed9a08751057fb2608e21b04298bd00f89f
Tad Hardesty <tad@platymuus.com>
2021-08-14 13:24:31 -0700

    Fix unknown gnu_printf warnings on Emscripten

--- source/ham/log.h
+++ source/ham/log.h
@@ -3,16 +3,20 @@
 
 // Conditionals for warnings/analysis based on SDL_stdinc.h
 #ifdef __GNUC__
-#define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( gnu_printf, fmtargnumber, fmtargnumber+1 )))
+#	ifdef __EMSCRIPTEN__
+#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( printf, fmtargnumber, fmtargnumber+1 )))
+#	else
+#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( gnu_printf, fmtargnumber, fmtargnumber+1 )))
+#	endif
 #else  // __GNUC__
-#define PRINTF_FUNC( fmtargnumber )
+#	define PRINTF_FUNC( fmtargnumber )
 #endif  // __GNUC__
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1600)
-#include <sal.h>
-#define PRINTF_FMT _Printf_format_string_
+#	include <sal.h>
+#	define PRINTF_FMT _Printf_format_string_
 #else  // _MSC_VER
-#define PRINTF_FMT
+#	define PRINTF_FMT
 #endif  // _MSC_VER
 
 // Debug and error logging functions
@@ -23,11 +27,11 @@ void LogError(PRINTF_FMT const char* fmt, ...) PRINTF_FUNC(1);
 #undef PRINTF_FMT
 
 #ifdef LOG
-#define DBG(X) LogDebug("%s", X)
-#define PrintToLog(A, B) LogDebug("%s : %d", A, B)
+#	define DBG(X) LogDebug("%s", X)
+#	define PrintToLog(A, B) LogDebug("%s : %d", A, B)
 #else  // LOG
-#define DBG(X)
-#define PrintToLog(A, B)
+#	define DBG(X)
+#	define PrintToLog(A, B)
 #endif  // LOG
 
 #endif  // LOG_H
