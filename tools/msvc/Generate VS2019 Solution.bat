7eb3920b3a27a41ffb4057519f85a12c1f9d7289
Tad Hardesty <tad@platymuus.com>
2021-08-24 03:33:08 -0700

    Create installers/ and appdata/ for VS

--- tools/msvc/Generate VS2019 Solution.bat
+++ tools/msvc/Generate VS2019 Solution.bat
@@ -3,4 +3,13 @@ cd /D "%~dp0\..\.."
 call powershell.exe -NoLogo -ExecutionPolicy Bypass -File "tools\msvc\install-dependencies.ps1" %*
 echo ==== Generating solution ====
 call build\premake5.exe vs2019
+rem Make the various directories that instructions will call for
+mkdir installers 2>nul
+mkdir appdata 2>nul
+mkdir appdata\lunatic 2>nul
+mkdir appdata\supreme 2>nul
+mkdir appdata\sleepless 2>nul
+mkdir appdata\mystic 2>nul
+mkdir appdata\loonyland 2>nul
+mkdir appdata\loonyland2 2>nul
 pause
