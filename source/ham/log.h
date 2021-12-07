6cd0649ffa6b447d7b88e8619091fdd2b596d945
Tad Hardesty <tad@platymuus.com>
2021-12-06 22:30:04 -0800

    Use SDL2's vararg defines instead of custom ones

--- source/ham/log.h
+++ source/ham/log.h
@@ -1,30 +1,11 @@
 #ifndef LOG_H
 #define LOG_H
 
-// Conditionals for warnings/analysis based on SDL_stdinc.h
-#ifdef __GNUC__
-#	if defined(__EMSCRIPTEN__) || (defined(__ANDROID__) && __ANDROID__)
-#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( printf, fmtargnumber, fmtargnumber+1 )))
-#	else
-#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( gnu_printf, fmtargnumber, fmtargnumber+1 )))
-#	endif
-#else  // __GNUC__
-#	define PRINTF_FUNC( fmtargnumber )
-#endif  // __GNUC__
-
-#if defined(_MSC_VER) && (_MSC_VER >= 1600)
-#	include <sal.h>
-#	define PRINTF_FMT _Printf_format_string_
-#else  // _MSC_VER
-#	define PRINTF_FMT
-#endif  // _MSC_VER
+#include <SDL_stdinc.h>
 
 // Debug and error logging functions
-void LogDebug(PRINTF_FMT const char* fmt, ...) PRINTF_FUNC(1);
-void LogError(PRINTF_FMT const char* fmt, ...) PRINTF_FUNC(1);
-
-#undef PRINTF_FUNC
-#undef PRINTF_FMT
+void LogDebug(SDL_PRINTF_FORMAT_STRING const char* fmt, ...) SDL_PRINTF_VARARG_FUNC(1);
+void LogError(SDL_PRINTF_FORMAT_STRING const char* fmt, ...) SDL_PRINTF_VARARG_FUNC(1);
 
 #ifdef LOG
 #	define DBG(X) LogDebug("%s", X)
