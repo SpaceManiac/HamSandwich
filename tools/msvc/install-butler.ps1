51802ebb095b66af6245b4223a64172e5a180a10
Tad Hardesty <tad@platymuus.com>
2021-12-08 16:31:57 -0800

    Pin butler version and add Linux/macOS scripts

--- tools/msvc/install-butler.ps1
+++ tools/msvc/install-butler.ps1
@@ -6,11 +6,12 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 if (-not (Test-Path "build/butler.exe")) {
 	New-Item build -ItemType Directory -ErrorAction SilentlyContinue > $null
 
-	Write-Output "==== Downloading Butler ===="
+	Write-Output "==== Downloading butler binary ===="
 	Invoke-WebRequest `
-		-Uri "https://broth.itch.ovh/butler/windows-amd64/LATEST/archive/default" `
+		-Uri "https://broth.itch.ovh/butler/windows-amd64/15.21.0/archive/default" `
 		-OutFile "build/butler.zip" `
 		-ErrorAction Stop
+	CheckHash -File "build/butler.zip" -Sha "e5381a1ec38abe8c2f3bf742dc22e897901e7def22d20ebc8fd4490d52038d35"
 
 	[System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/build/butler.zip", "$PWD/build")
 	rm build/butler.zip -ErrorAction Stop
