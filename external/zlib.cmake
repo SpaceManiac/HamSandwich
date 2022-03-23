73d71d5952296b165ea08c98b154fc71b449dc10
Tad Hardesty <tad@platymuus.com>
2022-03-23 01:03:36 -0700

    Add ZipVfs class for all_supreme_worlds.zip

--- external/zlib.cmake
+++ external/zlib.cmake
@@ -9,3 +9,11 @@ elseif(MSVC)
 	target_link_libraries(z INTERFACE zlibstatic)
 	target_include_directories(z INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/zlib" "${CMAKE_CURRENT_BINARY_DIR}/zlib")
 endif()
+
+add_library(minizip STATIC
+	"${CMAKE_CURRENT_SOURCE_DIR}/zlib/contrib/minizip/unzip.c"
+	"${CMAKE_CURRENT_SOURCE_DIR}/zlib/contrib/minizip/ioapi.c")
+target_include_directories(minizip PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/zlib/contrib/minizip")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+	target_compile_options(minizip PRIVATE -fPIC)
+endif()
--- source/ham/appdata.cpp
+++ source/ham/appdata.cpp
@@ -27,6 +27,8 @@
 #include <SDL_platform.h>
 #include <SDL_rwops.h>
 #include <SDL_messagebox.h>
+#include <unzip.h>
+#include <vec_rw.h>
 
 /*
 The interface in appdata.h currently distinguishes between AppdataOpen
@@ -354,6 +356,104 @@ FILE* fp_from_bundle(const char* file, const char* mode, SDL_RWops* rw, const ch
 }
 
 // ----------------------------------------------------------------------------
+// Zip VFS implementation
+
+class ZipVfs : public Vfs
+{
+	unzFile zip;
+	ZipVfs(const ZipVfs& other) = delete;
+	ZipVfs(ZipVfs&& other) = delete;
+	ZipVfs& operator=(const ZipVfs& other) = delete;
+	ZipVfs& operator=(ZipVfs&& other) = delete;
+public:
+	ZipVfs(const char* fname) : zip(unzOpen(fname)) {}
+	~ZipVfs();
+
+	FILE* open_stdio(const char* filename);
+	SDL_RWops* open_sdl(const char* filename);
+	bool list_dir(const char* directory, std::set<std::string>& output);
+};
+
+ZipVfs::~ZipVfs()
+{
+	unzClose(zip);
+}
+
+FILE* ZipVfs::open_stdio(const char* filename)
+{
+	SDL_RWops* rw = open_sdl(filename);
+	return rw ? fp_from_bundle(filename, "rb", rw, ".zip_tmp", true) : nullptr;
+}
+
+SDL_RWops* ZipVfs::open_sdl(const char* filename)
+{
+	if (unzLocateFile(zip, filename, 0) != UNZ_OK)
+		return nullptr;
+	unz_file_info64 file_info;
+	if (unzGetCurrentFileInfo64(zip, &file_info, nullptr, 0, nullptr, 0, nullptr, 0) != UNZ_OK)
+	{
+		LogError("ZipVfs::open_sdl(%s): bad unzGetCurrentFileInfo64", filename);
+		return nullptr;
+	}
+	if (unzOpenCurrentFile(zip) != UNZ_OK)
+	{
+		LogError("ZipVfs::open_sdl(%s): bad unzOpenCurrentFile", filename);
+		return nullptr;
+	}
+	std::vector<uint8_t> buffer(file_info.uncompressed_size);
+	if (unzReadCurrentFile(zip, buffer.data(), file_info.uncompressed_size) < 0)
+	{
+		LogError("ZipVfs::open_sdl(%s): bad unzReadCurrentFile", filename);
+		return nullptr;
+	}
+	unzCloseCurrentFile(zip);
+	return vanilla::create_vec_rwops(std::move(buffer));
+}
+
+bool ZipVfs::list_dir(const char* directory_raw, std::set<std::string>& output)
+{
+	std::string_view directory = directory_raw;
+
+	unz_global_info64 gi;
+	if (unzGetGlobalInfo64(zip, &gi) != UNZ_OK)
+	{
+		LogError("ZipVfs::list_dir(): bad unzGetGlobalInfo64");
+		return false;
+	}
+	if (unzGoToFirstFile(zip) != UNZ_OK)
+	{
+		LogError("ZipVfs::list_dir(): bad unzGoToFirstFile");
+		return false;
+	}
+	for (size_t i = 0; i < gi.number_entry; ++i)
+	{
+		unz_file_info64 file_info;
+		char filename_buf[1024];
+		if (unzGetCurrentFileInfo64(zip, &file_info, filename_buf, sizeof(filename_buf), nullptr, 0, nullptr, 0) != UNZ_OK)
+		{
+			LogError("ZipVfs::list_dir(): bad unzGetCurrentFileInfo64");
+			return false;
+		}
+
+		std::string_view filename(filename_buf, file_info.size_filename);
+		if (filename.substr(0, directory.size()) == directory && filename[directory.size()] == '/')
+		{
+			output.insert(std::string(filename.substr(directory.size() + 1)));
+		}
+
+		if (i + 1 < gi.number_entry)
+		{
+			if (unzGoToNextFile(zip) != UNZ_OK)
+			{
+				LogError("ZipVfs::list_dir(): bad unzGoToNextFile");
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+// ----------------------------------------------------------------------------
 // NSIS VFS implementation
 
 class NsisVfs : public Vfs {
@@ -413,6 +513,8 @@ static Mount init_vfs_spec(const char* what, const char* spec) {
 
 	if (!strcmp(kind, "stdio")) {
 		return { std::make_unique<StdioVfs>(param), mountpoint };
+	} else if (!strcmp(kind, "zip")) {
+		return { std::make_unique<ZipVfs>(param), mountpoint };
 	} else if (!strcmp(kind, "nsis")) {
 		SDL_RWops* fp = SDL_RWFromFile(param, "rb");
 		if (!fp) {
