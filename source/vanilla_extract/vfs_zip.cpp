cb6557596a5856ee297d01c5bb47aeeaa9612e4c
Tad Hardesty <tad@platymuus.com>
2022-03-26 02:03:25 -0700

    Android: use SDL_RWops when unzipping

--- source/vanilla_extract/vfs_zip.cpp
+++ source/vanilla_extract/vfs_zip.cpp
@@ -6,6 +6,64 @@
 using namespace vanilla;
 
 // ----------------------------------------------------------------------------
+// For Android's benefit, a wrapper from SDL_RWops to zip i/o
+
+static void* zsdl_open64(void* userdata, const void* filename, int mode)
+{
+	return SDL_RWFromFile((const char*) filename, "rb");
+}
+
+static uLong zsdl_read(void* userdata, void* stream, void* buf, uLong size)
+{
+	return SDL_RWread((SDL_RWops*) stream, buf, 1, size);
+}
+
+static uLong zsdl_write(void* userdata, void* stream, const void* buf, uLong size)
+{
+	return SDL_RWwrite((SDL_RWops*) stream, buf, 1, size);
+}
+
+static ZPOS64_T zsdl_tell64(void* userdata, void* stream)
+{
+	return SDL_RWtell((SDL_RWops*) stream);
+}
+
+static long zsdl_seek64(void* userdata, void* stream, ZPOS64_T offset, int origin)
+{
+	int sdl_whence = RW_SEEK_SET;
+	switch (origin)
+	{
+		case ZLIB_FILEFUNC_SEEK_SET: sdl_whence = RW_SEEK_SET; break;
+		case ZLIB_FILEFUNC_SEEK_CUR: sdl_whence = RW_SEEK_CUR; break;
+		case ZLIB_FILEFUNC_SEEK_END: sdl_whence = RW_SEEK_END; break;
+	}
+	SDL_RWseek((SDL_RWops*) stream, offset, sdl_whence);
+	return 0;
+}
+
+static int zsdl_close(void* userdata, void* stream)
+{
+	return SDL_RWclose((SDL_RWops*) stream);
+}
+
+static int zsdl_testerror(void* userdata, void* stream)
+{
+	return 0;
+}
+
+static zlib_filefunc64_def SDL_ZLIB_IO =
+{
+	zsdl_open64,
+	zsdl_read,
+	zsdl_write,
+	zsdl_tell64,
+	zsdl_seek64,
+	zsdl_close,
+	zsdl_testerror,
+	nullptr,
+};
+
+// ----------------------------------------------------------------------------
 // Zip VFS implementation
 
 class ZipVfs : public Vfs
@@ -16,7 +74,7 @@ class ZipVfs : public Vfs
 	ZipVfs& operator=(const ZipVfs& other) = delete;
 	ZipVfs& operator=(ZipVfs&& other) = delete;
 public:
-	ZipVfs(const char* fname) : zip(unzOpen(fname)) {}
+	ZipVfs(const char* fname) : zip(unzOpen2_64(fname, &SDL_ZLIB_IO)) {}
 	~ZipVfs();
 
 	SDL_RWops* open_sdl(const char* filename);
