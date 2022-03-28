78ce2189e7fa431c1479a0724bc1645f0eb91087
Tad Hardesty <tad@platymuus.com>
2022-03-27 17:34:57 -0700

    Bring back custom printf lint defines
    So that we don't get warnings about "%zu" being unknown.
    
    This reverts commit 6cd0649ffa6b447d7b88e8619091fdd2b596d945.

--- source/ham/log.h
+++ source/ham/log.h
@@ -1,12 +1,32 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <SDL_stdinc.h>
+// Conditionals for warnings/analysis based on SDL_stdinc.h
+// The difference is we use `gnu_printf` instead of just `printf`.
+#ifdef __GNUC__
+#	if defined(__EMSCRIPTEN__) || (defined(__ANDROID__) && __ANDROID__) || defined(__MACOSX__)
+#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( printf, fmtargnumber, fmtargnumber+1 )))
+#	else
+#		define PRINTF_FUNC( fmtargnumber ) __attribute__ (( format( gnu_printf, fmtargnumber, fmtargnumber+1 )))
+#	endif
+#else  // __GNUC__
+#	define PRINTF_FUNC( fmtargnumber )
+#endif  // __GNUC__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+#	include <sal.h>
+#	define PRINTF_FMT _Printf_format_string_
+#else  // _MSC_VER
+#	define PRINTF_FMT
+#endif  // _MSC_VER
 
 // Debug and error logging functions
 void LogInit();
-void LogDebug(SDL_PRINTF_FORMAT_STRING const char* fmt, ...) SDL_PRINTF_VARARG_FUNC(1);
-void LogError(SDL_PRINTF_FORMAT_STRING const char* fmt, ...) SDL_PRINTF_VARARG_FUNC(1);
+void LogDebug(PRINTF_FMT const char* fmt, ...) PRINTF_FUNC(1);
+void LogError(PRINTF_FMT const char* fmt, ...) PRINTF_FUNC(1);
+
+#undef PRINTF_FUNC
+#undef PRINTF_FMT
 
 #ifdef LOG
 #	define DBG(X) LogDebug("%s", X)
