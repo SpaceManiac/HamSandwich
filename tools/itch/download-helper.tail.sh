de4a0915facea07f540164f09660a972f72b89fe
Tad Hardesty <tad@platymuus.com>
2022-03-29 23:07:41 -0700

    Remove old build system

--- tools/itch/download-helper.tail.sh
+++
@@ -1,3 +0,0 @@
-
-project="$1" config=release_x86_64 debug_configure
-iterate_installers do_it
--- tools/msvc/Create Release Zip.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /D "%~dp0\..\.."
-call powershell.exe -NoLogo -ExecutionPolicy Bypass -File "tools\msvc\build-binary-package.ps1" -Zip 1 %*
-pause
