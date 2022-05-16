69b24c0a686bd5ea094a8928699e7c2e142a720a
Tad Hardesty <tad@platymuus.com>
2022-05-16 00:16:28 -0700

    Restore default control bindings for Eddie and Spooky Castle

--- source/spookycastle/main.cpp
+++ source/spookycastle/main.cpp
@@ -36,6 +36,15 @@ TASK(int) main(int argc, char* argv[])
 	HAM_EXTERN_FULFILL
 	AppdataInit();
 
+	byte kb0[6] = { SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT, SDL_SCANCODE_LCTRL, SDL_SCANCODE_LSHIFT };
+	byte kb1[6] = { SDL_SCANCODE_KP_8, SDL_SCANCODE_KP_2, SDL_SCANCODE_KP_4, SDL_SCANCODE_KP_6, SDL_SCANCODE_Z, SDL_SCANCODE_X };
+	byte kb2[6] = { 0, 0, 0, 0, SDL_SCANCODE_RETURN, 0 };
+	byte joyBinds[2] = { 0, 1 };
+	SetKeyboardBindings(0, 6, kb0);
+	SetKeyboardBindings(1, 6, kb1);
+	SetKeyboardBindings(2, 6, kb2);
+	SetJoystickBindings(2, joyBinds);
+
 	bool windowedGame=false;
 
 	for (int i = 1; i < argc; ++i)
