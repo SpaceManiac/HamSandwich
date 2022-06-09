c861bf76864965ebbf66b4a73bdbf8099d3bd1dc
Hypexion <hypexionprime@gmail.com>
2022-06-09 12:09:48 +0100

    Move LL2 title screen defines to loonyland2/title.cpp

--- source/loonyland2/title.cpp
+++ source/loonyland2/title.cpp
@@ -20,6 +20,43 @@
 #define VERSION_NO	"Version 1.0O"
 #endif
 
+#ifdef DIRECTORS
+#define MM_PLAY			0
+#define MM_ERASE		1
+#define MM_ACHIEVE		2
+#define MM_GALLERY		3
+#define MM_EDITOR		4
+#define MM_OPTIONS		5
+#define MM_QUIT			6
+#define MENU_CHOICES	7
+#else
+#define MM_PLAY			0
+#define MM_ERASE		1
+#define MM_ACHIEVE		2
+#define MM_OPTIONS		3
+#define MM_QUIT			4
+#define MENU_CHOICES	5
+#endif
+
+#define MENU_MODE_NORMAL	0
+#define MENU_MODE_ERASE		1
+#define MENU_MODE_NEW		2
+
+#define SC_NONE			0
+
+#define SC_YES			0
+#define SC_NO			1
+
+#define SC_PLAYER_NAME	0
+#define SC_MODIFIER_1	1
+#define SC_MODIFIER_2	2
+#define SC_MODIFIER_3	3
+#define SC_START_GAME	4
+
+// Modifer positions must be contiguous to work correctly
+static_assert(SC_MODIFIER_2 == SC_MODIFIER_1 + 1);
+static_assert(SC_MODIFIER_3 == SC_MODIFIER_1 + 2);
+
 #pragma pack(push, 1)
 typedef struct save_t
 {
