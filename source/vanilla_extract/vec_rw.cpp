ce06c90f0b7ee2e466f4195607ae3b4747b78fb6
Tad Hardesty <tad@platymuus.com>
2023-03-08 18:06:20 -0800

    Clarify Vec_RWops implementation somewhat

--- source/vanilla_extract/vec_rw.cpp
+++ source/vanilla_extract/vec_rw.cpp
@@ -2,38 +2,46 @@
 
 #include <SDL_rwops.h>
 
-namespace vanilla {
+namespace vanilla
+{
 
-struct Vec_RWops {
-	SDL_RWops base;
-	std::vector<uint8_t> vec;
-};
-
-static int delete_vec_close(SDL_RWops* rw) {
-	if (rw) {
-		Vec_RWops* rw2 = (Vec_RWops*) rw;
-		rw2->~Vec_RWops();
-		SDL_FreeRW(rw);
-	}
-	return 0;
-}
-
-owned::SDL_RWops create_vec_rwops(std::vector<uint8_t>&& buffer) {
-	// Use a modified RWFromConstMem so the JS audio code knows how to slurp it
-	SDL_RWops* rw_base = SDL_RWFromConstMem(buffer.data(), buffer.size());
-	if (!rw_base)
+	struct Vec_RWops
 	{
-		// SDL_RWFromConstMem returns null for size 0 buffers. It would make
-		// more sense to return an empty stream IMO, but we should at least
-		// not crash.
-		return nullptr;
-	}
-	Vec_RWops* rw = (Vec_RWops*) SDL_malloc(sizeof(Vec_RWops));
-	rw->base = *rw_base;
-	SDL_FreeRW(rw_base);
-	new(&rw->vec) std::vector<uint8_t>(std::move(buffer));
-	rw->base.close = delete_vec_close;
-	return owned::SDL_RWops { &rw->base };
-}
+		SDL_RWops base;
+		std::vector<uint8_t> vec;
 
-}  // namespace vanilla
+		void *operator new(size_t size)
+		{
+			return SDL_malloc(size);
+		}
+
+		void operator delete(void *ptr)
+		{
+			return SDL_free(ptr);
+		}
+	};
+
+	static int delete_vec_close(SDL_RWops *rw)
+	{
+		delete (Vec_RWops *)rw;
+		return 0;
+	}
+
+	owned::SDL_RWops create_vec_rwops(std::vector<uint8_t> &&buffer)
+	{
+		// Use a modified RWFromConstMem so the JS audio code knows how to slurp it.
+		SDL_RWops *rw_base = SDL_RWFromConstMem(buffer.data(), buffer.size());
+		if (!rw_base)
+		{
+			// SDL_RWFromConstMem returns null for size 0 buffers. It would make
+			// more sense to return an empty stream IMO, but we should at least
+			// not crash.
+			return nullptr;
+		}
+		Vec_RWops *rw = new Vec_RWops{*rw_base, std::move(buffer)};
+		SDL_FreeRW(rw_base);
+		rw->base.close = delete_vec_close;
+		return owned::SDL_RWops{&rw->base};
+	}
+
+} // namespace vanilla
