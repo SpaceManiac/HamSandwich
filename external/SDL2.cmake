769a283f83016055c577f188de4496d61b1c119b
Tad Hardesty <tad@platymuus.com>
2023-08-18 16:14:44 -0700

    Update Win and Mac versions of SDL2 and SDL2_image

--- external/SDL2.cmake
+++ external/SDL2.cmake
@@ -20,10 +20,12 @@ elseif(ANDROID)
 	set(ANDROID_OBJDIR "${CMAKE_CURRENT_SOURCE_DIR}/../build/android/SDL2/intermediates/ndkBuild/${ANDROID_BUILD_TYPE}/obj/local/${ANDROID_ABI}")
 
 	target_include_directories(SDL2 INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/SDL2/include")
-	target_include_directories(SDL2_image INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_image")
-	target_include_directories(SDL2_mixer INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_mixer")
 	target_link_libraries(SDL2 INTERFACE "${ANDROID_OBJDIR}/libSDL2.so")
+
+	target_include_directories(SDL2_image INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_image")
 	target_link_libraries(SDL2_image INTERFACE "${ANDROID_OBJDIR}/libSDL2_image.so")
+
+	target_include_directories(SDL2_mixer INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_mixer")
 	target_link_libraries(SDL2_mixer INTERFACE "${ANDROID_OBJDIR}/libSDL2_mixer.so")
 elseif(WIN32)
 	# On MSVC, use the official prebuilt binaries.
@@ -34,15 +36,13 @@ elseif(WIN32)
 	endif()
 
 	include(FetchContent)
-	# SDL 2.0.8 is the real pinned version, but SDL 2.0.9 fixes a Windows-only bug:
-	# https://github.com/libsdl-org/SDL/commit/c04dca0dad2696d7dcd51427c8183b19ebff8a60
 	FetchContent_Declare(SDL2
-		URL https://www.libsdl.org/release/SDL2-devel-2.0.9-VC.zip
-		URL_HASH SHA256=ea266ef613f88433f493498f9e72e6bed5d03e4f3fde5b571a557a754ade9065
+		URL https://www.libsdl.org/release/SDL2-devel-2.0.22-VC.zip
+		URL_HASH SHA256=32adc96d8b25e5671189f1f38a4fc7deb105fbb1b3ed78ffcb23f5b8f36b3922
 	)
 	FetchContent_Declare(SDL2_image
-		URL https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.4-VC.zip
-		URL_HASH SHA256=4e15fad9de43d738b476e422eef1910432443cead60d2084b3ef01d3f4a76087
+		URL https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip
+		URL_HASH SHA256=a180f9b75c4d3fbafe02af42c42463cc7bc488e763cfd1ec2ffb75678b4387ac
 	)
 	FetchContent_Declare(SDL2_mixer
 		URL https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.4-VC.zip
@@ -92,16 +92,16 @@ elseif(APPLE)
 	# Like Windows, use the official SDL2 prebuild binaries.
 	include(FetchContent)
 	FetchContent_Declare(SDL2
-		URL https://www.libsdl.org/release/SDL2-2.0.8.dmg
-		URL_HASH SHA256=74dd2cb6b18e35e8181523590115f10f1da774939c21ce27768a2a80ba57ad5f
+		URL https://www.libsdl.org/release/SDL2-2.0.22.dmg
+		URL_HASH SHA256=72974672b8359057aa2f6d467c8adae8182a6caedd660e3936e23c3c683c3801
 		DOWNLOAD_NO_EXTRACT TRUE
 		PATCH_COMMAND hdiutil mount <DOWNLOADED_FILE> -mountpoint dmg_mount
 		COMMAND cp -r dmg_mount dmg_content
 		COMMAND hdiutil unmount dmg_mount
 	)
 	FetchContent_Declare(SDL2_image
-		URL https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.4.dmg
-		URL_HASH SHA256=7efbf1733ba02d5ce278ae27b0e35baeb6d0eaf0e5f56f187e608681aec98b39
+		URL https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.dmg
+		URL_HASH SHA256=313b2b92544bc05cafeb7c566572d08c961f15f25877608ebf86658bd458d815
 		DOWNLOAD_NO_EXTRACT TRUE
 		PATCH_COMMAND hdiutil mount <DOWNLOADED_FILE> -mountpoint dmg_mount
 		COMMAND cp -r dmg_mount dmg_content
