c6c73d1b6d6f6e7271c3f5c5dd55231539f52b4c
Tad Hardesty <tad@platymuus.com>
2023-08-27 18:36:53 -0700

    Add gamepad support to Record Book

--- source/supreme/recordbook.cpp
+++ source/supreme/recordbook.cpp
@@ -17,11 +17,14 @@
 #define PBTN_HEIGHT	19
 
 static byte *backgd;
-static sprite_set_t *plSpr;
+static std::unique_ptr<sprite_set_t> plSpr;
 static char msBright,msDBright;
 static int msx,msy;
 static dword totalKills,lvlsPassed;
 static float shopPct,playPct,scanPct,gamePct;
+static int currentGoal;
+static byte oldc;
+static bool mouseMode;
 
 float CalcPlayPercent(void)
 {
@@ -61,7 +64,7 @@ void InitRecordBook(MGLDraw *mgl)
 
 	mgl->LoadBMP("graphics/profmenu.bmp");
 	backgd=(byte *)malloc(640*480);
-	plSpr=new sprite_set_t("graphics/pause.jsp");
+	plSpr = std::make_unique<sprite_set_t>("graphics/pause.jsp");
 
 	for(i=0;i<480;i++)
 		memcpy(&backgd[i*640],&mgl->GetScreen()[i*mgl->GetWidth()],640);
@@ -88,19 +91,25 @@ void InitRecordBook(MGLDraw *mgl)
 		if(profile.progress.goal[i])
 			t++;
 	gamePct+=0.1f*((float)t);	// 10% total game progress for gallery goals
+
+	currentGoal = 0;
+	oldc = ~0;
 }
 
 void ExitRecordBook(void)
 {
 	free(backgd);
-	delete plSpr;
+	plSpr.reset();
 }
 
 byte UpdateRecordBook(int *lastTime,MGLDraw *mgl)
 {
 	char k;
 
+	int oldMsx = msx, oldMsy = msy;
 	mgl->GetMouse(&msx,&msy);
+	if (msx != oldMsx || msy != oldMsy)
+		mouseMode = true;
 
 	if(*lastTime>TIME_PER_FRAME*5)
 		*lastTime=TIME_PER_FRAME*5;
@@ -121,6 +130,7 @@ byte UpdateRecordBook(int *lastTime,MGLDraw *mgl)
 		mgl->Process();
 		*lastTime-=TIME_PER_FRAME;
 	}
+
 	if(mgl->MouseTap())
 	{
 		if(PointInRect(msx,msy,560,440,560+60,440+PBTN_HEIGHT))
@@ -128,6 +138,38 @@ byte UpdateRecordBook(int *lastTime,MGLDraw *mgl)
 			return 1;
 		}
 	}
+
+	if (GetGamepadButtons() & ((1 << SDL_CONTROLLER_BUTTON_B) | (1 << SDL_CONTROLLER_BUTTON_BACK)))
+	{
+		return 1;
+	}
+
+	byte c = GetControls() | GetArrows();
+	if (c & ~oldc & CONTROL_UP)
+	{
+		mouseMode = false;
+		if (currentGoal >= 25)
+			currentGoal -= 25;
+	}
+	if (c & ~oldc & CONTROL_DN)
+	{
+		mouseMode = false;
+		if (currentGoal < 75)
+			currentGoal += 25;
+	}
+	if (c & ~oldc & CONTROL_LF)
+	{
+		mouseMode = false;
+		if ((currentGoal % 25) > 0)
+			currentGoal -= 1;
+	}
+	if (c & ~oldc & CONTROL_RT)
+	{
+		mouseMode = false;
+		if ((currentGoal % 25) < 24)
+			currentGoal += 1;
+	}
+	oldc = c;
 
 	return 0;
 }
@@ -245,10 +287,10 @@ void RenderRecordBook(MGLDraw *mgl)
 	PrintGlow(20,297,"Gallery Goals Completed",0,2);
 	for(i=0;i<100;i++)
 	{
-		if(PointInRect(msx,msy,msx2,msy2,msx2+20,msy2+20))
+		if(PointInRect(msx,msy,msx2,msy2,msx2+20,msy2+20) || currentGoal == i)
 		{
 			DrawBox(msx2,msy2,msx2+20,msy2+20,32*1+31);
-			PrintGoalInfo(20,412,i);
+			currentGoal = i;
 		}
 		else
 			DrawBox(msx2,msy2,msx2+20,msy2+20,32*1+16);
@@ -263,20 +305,25 @@ void RenderRecordBook(MGLDraw *mgl)
 		}
 	}
 
+	PrintGoalInfo(20, 412, currentGoal);
+
 	// mouse cursor
-	SetSpriteConstraints(13,13,627,467);
-	msx2=msx;
-	msy2=msy;
-	if(msx2<13)
-		msx2=13;
-	if(msy2<13)
-		msy2=13;
-	if(msx2>622)
-		msx2=622;
-	if(msy2>462)
-		msy2=462;
-	plSpr->GetSprite(0)->DrawBright(msx2,msy2,mgl,msBright/2);
-	ClearSpriteConstraints();
+	if (mouseMode)
+	{
+		SetSpriteConstraints(13,13,627,467);
+		msx2=msx;
+		msy2=msy;
+		if(msx2<13)
+			msx2=13;
+		if(msy2<13)
+			msy2=13;
+		if(msx2>622)
+			msx2=622;
+		if(msy2>462)
+			msy2=462;
+		plSpr->GetSprite(0)->DrawBright(msx2,msy2,mgl,msBright/2);
+		ClearSpriteConstraints();
+	}
 }
 
 //----------------
