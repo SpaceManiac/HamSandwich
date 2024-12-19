711a11731026e9bdece3ca42259ac9c752f65b64
Tad Hardesty <tad@platymuus.com>
2024-12-18 23:03:47 -0800

    aegea: Add sketch of writing to data storage

--- source/aegea/include/aegea.h
+++ source/aegea/include/aegea.h
@@ -1,24 +1,47 @@
 #ifndef AEGEA_H
 #define AEGEA_H
 
-#include <string>
+#include <map>
 #include <memory>
+#include <string>
 #include <string_view>
+#include <vector>
+
+namespace jt
+{
+	class Json;
+}
 
 class WebSocket;
 
 class ArchipelagoClient
 {
+	enum {
+		WaitingForRoomInfo,
+		WaitingForDataPackage,
+		WaitingForConnected,
+		Active,
+	} status = WaitingForRoomInfo;
+
 	std::string game, address, slot, password;
 	std::string error;
 	std::unique_ptr<WebSocket> socket;
+	std::vector<jt::Json> outgoing;
+
+	std::map<std::string, jt::Json> storage;
+	std::map<std::string, jt::Json> storageChanges;
 public:
 	ArchipelagoClient(std::string_view game, std::string_view address, std::string_view slot, std::string_view password);
 	~ArchipelagoClient();
 
-	std::string_view error_message();
+	std::string_view error_message() const;
+	std::string_view connection_status() const;
+	bool is_active() const;
 
+	// Call this on a regular basis (ex: once per frame) to handle networking.
 	void update();
+
+	void storage_set(std::string_view key, jt::Json value, bool want_reply = true);
 };
 
 #endif
--- source/aegea/src/aegea.cpp
+++ source/aegea/src/aegea.cpp
@@ -30,52 +30,85 @@ ArchipelagoClient::~ArchipelagoClient()
 	// Declaring and defining this lets good things happen.
 }
 
-std::string_view ArchipelagoClient::error_message()
+std::string_view ArchipelagoClient::error_message() const
 {
 	return !error.empty() ? error : socket ? socket->error_message() : "no socket";
 }
 
+std::string_view ArchipelagoClient::connection_status() const
+{
+	if (!socket)
+	{
+		return "Not connected";
+	}
+	else if (!socket->is_connected())
+	{
+		return "Connecting...";
+	}
+	else if (status == WaitingForRoomInfo)
+	{
+		return "Waiting for room info...";
+	}
+	else if (status == WaitingForDataPackage)
+	{
+		return "Downloading data package...";
+	}
+	else if (status == WaitingForConnected)
+	{
+		return "Authenticating...";
+	}
+	return "Active";
+}
+
+bool ArchipelagoClient::is_active() const
+{
+	return status == Active;
+}
+
 void ArchipelagoClient::update()
 {
-	if (!socket->error_message().empty())
+	if (!socket || !socket->error_message().empty())
 	{
 		return;
 	}
-
-	jt::Json outgoing;
-	outgoing.setArray();
 
 	while (const WebSocket::Message* message = socket->recv())
 	{
 		if (message->type == WebSocket::Close)
 		{
-			error = "WebSocket was closed by server";
+			error = "Server closed WebSocket connection";
 			return;
 		}
 
-		auto [status, body] = jt::Json::parse(message->data);
-		if (status != jt::Json::success)
+		auto [parseStatus, body] = jt::Json::parse(message->data);
+		if (parseStatus != jt::Json::success)
 		{
-			error = "Server sent invalid JSON (";
-			error += jt::Json::StatusToString(status);
-			error += ")";
+			error = "Server sent invalid JSON: ";
+			error += jt::Json::StatusToString(parseStatus);
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Server sent invalid JSON: %s in %s", jt::Json::StatusToString(parseStatus), message->data.c_str());
 			continue;
 		}
 		if (!body.isArray())
 		{
 			error = "Server sent non-array JSON";
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Server sent non-array JSON: %s", message->data.c_str());
 			continue;
 		}
 
-		fprintf(stderr, "<-- %s\n", message->data.c_str());
 		for (const auto& packet : body.getArray())
 		{
+#ifndef NDEBUG
+			fprintf(stderr, "<-- %s\n", packet.toString().c_str());
+#endif
+
 			const auto& cmdJ = packet["cmd"];
 			if (!cmdJ.isString())
 			{
+				error = "Server sent non-string \"cmd\"";
 				continue;
 			}
 			const auto& cmd = cmdJ.getString();
+			SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Archipelago: Handling %s", cmd.c_str());
 			if (cmd == "RoomInfo")
 			{
 				jt::Json connect;
@@ -91,19 +124,96 @@ void ArchipelagoClient::update()
 				connect["version"]["build"] = 1;
 				connect["items_handling"] = ItemsHandling::All;
 				connect["tags"][0] = "TextOnly";
-				outgoing.getArray().push_back(connect);
+				outgoing.push_back(connect);
+				status = WaitingForConnected;
 			}
 			else if (cmd == "ConnectionRefused")
 			{
-				error = "Connection refused: ";
+				error = "Server refused connection: ";
 				error += packet["errors"].toString();
+			}
+			else if (cmd == "Connected")
+			{
+				status = Active;
+			}
+			else if (cmd == "ReceivedItems")
+			{
+			}
+			else if (cmd == "LocationInfo")
+			{
+			}
+			else if (cmd == "RoomUpdate")
+			{
+			}
+			else if (cmd == "PrintJSON")
+			{
+			}
+			else if (cmd == "DataPackage")
+			{
+			}
+			else if (cmd == "Bounced")
+			{
+			}
+			else if (cmd == "InvalidPacket")
+			{
+				error = "Server rejected packet: ";
+				error += packet["text"].isString() ? packet["text"].getString() : packet.toString();
+			}
+			else if (cmd == "Retrieved")
+			{
+				if (packet["keys"].isObject())
+				{
+					const auto& object = packet["keys"].getObject();
+					storage.insert(object.begin(), object.end());
+				}
+			}
+			else if (cmd == "SetReply")
+			{
+				if (packet["key"].isString())
+				{
+					const std::string& key = packet["key"].getString();
+					storage[key] = packet["value"];
+					// If we don't already know an old value, save one now.
+					if (storageChanges.find(key) == storageChanges.end())
+					{
+						storageChanges[key] = packet["original_value"];
+					}
+				}
+			}
+			else
+			{
+				SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "Archipelago: Server sent unknown \"cmd\": %s", cmd.c_str());
 			}
 		}
 	}
 
-	if (!outgoing.getArray().empty())
+	if (socket && socket->is_connected() && !outgoing.empty())
 	{
-		fprintf(stderr, "--> %s\n", outgoing.toString().c_str());
-		socket->send_text(outgoing.toString().c_str());
+#ifndef NDEBUG
+		for (const auto& packet : outgoing)
+		{
+			fprintf(stderr, "--> %s\n", packet.toString().c_str());
+		}
+#endif
+
+		jt::Json outgoingJ;
+		auto& array = outgoingJ.setArray() = std::move(outgoing);
+		socket->send_text(outgoingJ.toString().c_str());
+		// Reuse vector
+		outgoing = std::move(array);
+		outgoing.clear();
+	}
+}
+
+void ArchipelagoClient::storage_set(std::string_view key, jt::Json value, bool want_reply)
+{
+	jt::Json& packet = outgoing.emplace_back();
+	packet["cmd"] = "Set";
+	packet["key"] = key;
+	packet["operations"][0]["operation"] = "replace";
+	packet["operations"][0]["value"] = value;
+	if (!want_reply) // True is the server-side default.
+	{
+		packet["want_reply"] = want_reply;
 	}
 }
