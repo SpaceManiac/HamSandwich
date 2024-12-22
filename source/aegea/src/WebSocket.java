105aa1b005b1b06798f0459c9322269786860a81
Tad Hardesty <tad@platymuus.com>
2024-12-21 17:39:27 -0800

    aegea: Connect to HTTP endpoint on Java side

--- source/aegea/src/WebSocket.java
+++ source/aegea/src/WebSocket.java
@@ -1,5 +1,22 @@
 package com.platymuus.aegea;
 
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class WebSocket {
 
     private static native void onConnect(long handle);
@@ -11,10 +28,43 @@ public class WebSocket {
     private static native void onClose(long handle, int reason);
 
     private long handle;
+    private final ConcurrentLinkedQueue<Runnable> callbacks = new ConcurrentLinkedQueue<>();
+    private final HttpURLConnection conn;
+    private boolean canWrite;
 
-    public WebSocket(long handle, String url) {
+    public WebSocket(long handle, String urlString) throws URISyntaxException, IOException, NoSuchAlgorithmException {
         this.handle = handle;
-        onConnect(handle);
+
+        Socket socket;
+        URI uri = new URI(urlString);
+
+        Log.d("Aegea", "uri ok: " + uri);
+
+        if (uri.getScheme().equals("ws")) {
+            URI uri2 = new URI("http", uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment());
+            Log.d("Aegea", "uri2 ws->http ok: " + uri2);
+            conn = (HttpURLConnection) uri2.toURL().openConnection();
+        } else if (uri.getScheme().equals("wss")) {
+            URI uri2 = new URI("https", uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment());
+            Log.d("Aegea", "uri2 wss->https ok: " + uri2);
+            conn = (HttpURLConnection) uri2.toURL().openConnection();
+        } else {
+            throw new URISyntaxException(urlString, "WebSocket URL must be ws:// or wss://");
+        }
+        Log.d("Aegea", "conn ok");
+
+        byte[] nonce = new byte[16];
+        new SecureRandom().nextBytes(nonce);
+
+        conn.setRequestProperty("Upgrade", "websocket");
+        conn.setRequestProperty("Connection", "Upgrade");
+        conn.setRequestProperty("Sec-WebSocket-Key", Base64.encodeToString(nonce, Base64.DEFAULT));
+        conn.setRequestProperty("Sec-WebSocket-Version", "13");
+        //conn.setDoInput(true);
+        //conn.setDoOutput(true);
+
+        Log.d("Aegea", "conn setup");
+        new Thread(() -> this.threadMain(conn), "WebSocket: " + urlString).start();
     }
 
     public void dispose() {
@@ -22,9 +72,34 @@ public class WebSocket {
     }
 
     public void recv() {
-        onError(handle, "Big Meme");
+        Runnable poll = callbacks.poll();
+        while (poll != null) {
+            poll.run();
+            poll = callbacks.poll();
+        }
     }
 
-    public void send(boolean text, byte[] data) {
+    public void send(boolean text, byte[] data) throws IOException {
+        if (canWrite) {
+            OutputStream os = conn.getOutputStream();
+        } else {
+            onError(handle, "send() too early");
+        }
+    }
+
+    private void threadMain(HttpURLConnection conn) {
+        try {
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            if (responseCode != 101) {
+                String message = conn.getResponseMessage();
+                callbacks.add(() -> onError(handle, "HTTP " + responseCode + " " + message));
+                return;
+            }
+
+            Log.d("Aegea", "upgraded to websocket ok");
+        } catch (Exception e) {
+            callbacks.add(() -> onError(handle, e.getMessage()));
+        }
     }
 }
