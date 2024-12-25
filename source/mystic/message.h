51a8ebd633de96132448e0f11d9d17e021c356c6
Tad Hardesty <tad@platymuus.com>
2024-12-25 00:06:37 -0800

    KM: Increase message size limit from 32 to 64

--- source/mystic/message.h
+++ source/mystic/message.h
@@ -4,16 +4,6 @@
 #include "winpch.h"
 #include "display.h"
 
-typedef struct message_t
-{
-	int x,y;
-	int dy;
-	int timer;
-	int bright;
-	char brightDir;
-	char msg[32];
-} message_t;
-
 void InitMessage(void);
 void NewMessage(const char *txt,int time);
 void NewBigMessage(const char *txt,int time);
