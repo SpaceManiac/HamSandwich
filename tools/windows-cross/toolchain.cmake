4f90cf487db9120db8e2b863bbbfe8c32ad38cc7
Tad Hardesty <tad@platymuus.com>
2025-02-18 10:40:38 -0800

    SWC: Enable Windows UTF-8 mode

--- tools/windows-cross/toolchain.cmake
+++ tools/windows-cross/toolchain.cmake
@@ -9,7 +9,7 @@ set(CMAKE_C_FLAGS_INIT "-fms-compatibility /imsvc/xwin/crt/include /imsvc/xwin/s
 set(CMAKE_CXX_FLAGS_INIT ${CMAKE_C_FLAGS_INIT})
 set(CMAKE_RC_FLAGS_INIT ${CMAKE_C_FLAGS_INIT})
 set(CMAKE_LINKER lld-link)
-set(CMAKE_MT llvm-mt)  # CMake can find rc=llvm-rc all by itself, but needs to be told mt=llvm-mt
+set(CMAKE_MT wrap-mt)
 set(CMAKE_EXE_LINKER_FLAGS_INIT "/LIBPATH:/xwin/crt/lib/x86 /LIBPATH:/xwin/sdk/lib/um/x86 /LIBPATH:/xwin/sdk/lib/ucrt/x86")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
 set(CMAKE_SHARED_LINKER_FLAGS_INIT ${CMAKE_EXE_LINKER_FLAGS_INIT})
