1aa568e1edaccc30e93d51c952f712776d232549
Tad Hardesty <tad@platymuus.com>
2025-02-23 21:44:29 -0800

    aseprite: Add basic Aseprite plugin which can import JSPs

---
+++ source/jspedit_aseprite/plugin.lua
@@ -0,0 +1,22 @@
+-- Plugin entry point. Registers commands which activate jspedit.lua on use.
+function init(plugin)
+	plugin:newCommand {
+		id = "jsp_import",
+		title = "Import JSP",
+		group = "file_import_1",
+		onclick = function()
+			dofile(app.fs.joinPath(plugin.path, "jspedit.lua")).import(plugin)
+		end
+	}
+
+	--[[
+	plugin:newCommand {
+		id = "jsp_export",
+		title = "Export JSP",
+		group = "file_export_1",
+		onclick = function()
+			dofile(app.fs.joinPath(plugin.path, "jspedit.lua")).export(plugin)
+		end
+	}
+	]]
+end
