e379c53d95b66966759b14111c9318d9c569ce14
Tad Hardesty <tad@platymuus.com>
2026-03-24 21:34:05 -0700

    Fix missing button images on Android

--- android-project/SDL2/Android.mk
+++ android-project/SDL2/Android.mk
@@ -1,18 +1,22 @@
 include ../../external/SDL2/Android.mk
 
-USE_STBIMAGE := true  # supports png and jpg
+# Get .png and .jpg support from STB
+USE_STBIMAGE := true
 SUPPORT_AVIF := false
 SUPPORT_JPG := false
 SUPPORT_JXL := false
-SUPPORT_PNG := false  # specific to libpng
+# SUPPORT_PNG is specific to libpng; we want USE_STBIMAGE
+SUPPORT_PNG := false
 SUPPORT_WEBP := false
 include ../../external/SDL2_image/Android.mk
 
 SUPPORT_WAV := true
 SUPPORT_FLAC_DRFLAC := false
 SUPPORT_FLAC_LIBFLAC := false
+# Get .ogg support from STB
 SUPPORT_OGG_STB := true
-SUPPORT_OGG := false  # specifically tremor + libogg
+# Not from tremor + libogg
+SUPPORT_OGG := false
 SUPPORT_MP3_MINIMP3 := false
 SUPPORT_MP3_MPG123 := false
 SUPPORT_WAVPACK := false
